50a6485074bfb7f0152678f5853d94736127b50e
Samet Yalcin <syalcin_@hotmail.com>
2019-03-22 15:21:47 +0100

    Location coordinate latitude and longitude

---
+++ GoodBuy/LocationManager.swift
@@ -0,0 +1,24 @@
+//
+//  LocationManager.swift
+//  GoodBuy
+//
+//  Created by Student on 22/03/2019.
+//  Copyright © 2019 Woensel. All rights reserved.
+//
+
+import UIKit
+
+class LocationManager: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GoodBuy/ViewController.swift
+++ GoodBuy/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // for use when the app is open
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//            locationManager.requestAlwaysAuthorization()
+            locationManager.requestLocation()
+            //locationManager.startUpdatingLocation()
+        }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first{
+            print(location.coordinate)
+            print("test")
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("x-error: \(error)")
+    }
+    
+   
 
-
+   
+    
 }
-
