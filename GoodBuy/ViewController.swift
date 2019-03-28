03868d50641bba432dbed6062c8b0eb2df9ca7e9
Samet Yalcin <syalcin_@hotmail.com>
2019-03-28 10:55:22 +0100

    Automatic location detect

--- GoodBuy/ViewController.swift
+++ GoodBuy/ViewController.swift
@@ -11,34 +11,62 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    let manager = CLLocationManager()
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations[0]
+        
+        //let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        //let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        //let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        //map.setRegion(region, animated: true)
+        
+        print(location.altitude)
+        print(location.speed)
+        
+        //self.map.showsUserLocation = true
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemark, error) in
+            if error != nil
+            {
+                print("THERE WAS AN ERROR")
+            }
+            else
+            {
+                if let place = placemark?[0]
+                {
+                    if let checker = place.locality
+                    {
+                        self.label.text = "\(place.locality!), \n \(place.country!) "
+                    }
+                    else
+                    {
+                        self.label.text = "NO SIGNAL"
+                    }
+                }
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // for use when the app is open
-        locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//            locationManager.requestAlwaysAuthorization()
-            locationManager.requestLocation()
-            //locationManager.startUpdatingLocation()
-        }
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        button.layer.cornerRadius = 10;
+        button.clipsToBounds = true;
+       
+       
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first{
-            print(location.coordinate)
-            print("test")
-            
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("x-error: \(error)")
-    }
+   
     
    
 
